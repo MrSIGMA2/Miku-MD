fcd9a5004d62270e49acfb900f550ef133ec575a
MrSIGMA2 <112796713+MrSIGMA2@users.noreply.github.com>
2022-12-03 16:11:18 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,20 +7,20 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.Owner = ['923188408174']
+global.OwnerNumber = ['923188408174']
+global.ownertag = ['923188408174']
+global.BotName = "SIGMA"
+global.packname = "SIGMA bot"
+global.author = "By: MR SIGMA"
+global.OwnerName = "MR SIGMA"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
 global.sessionName = "session"
 
 
 
-global.prefa = ['-'] // Prefix ---  change it to anythong you want except @//
+global.prefa = ['!'] // Prefix ---  change it to anythong you want except @//
 
 
 global.location = "West Bengal, India"
@@ -68,7 +68,7 @@ global.mess = {
     waiting: 'Chotto Matte...',
     nolink: 'Please provide me *link*, Baka!',
     error: 'An error occurd!',
-    banned: 'You are *Banned* fron using commands!',
+    banned: 'You are *Banned* from using commands!',
     bangc: 'This Group is *Banned* from using Commands!',
     nonsfw: 'Dont be a pervert Baka! This is not a NSFW enabled group!'
     
